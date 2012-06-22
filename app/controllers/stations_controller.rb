2d5c05cd74bde6379a62ab5c856f5c32cbaf25d6
Bradley Rees <sourcetree@bradleyrees.com>
2012-06-22 18:41:13 +0800

    Added page caching

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,42 +2,40 @@ require 'open-uri'
 require 'nokogiri'
 
 class StationsController < ApplicationController
-  def index
+  caches_page :all
+  def all
+      @result = Array.new
+      ["WA", "NSW", "VIC", "QLD", "NT", "TAS", "SA" ].sort().each do |state|
+        doc = Nokogiri::HTML(open("http://www.bom.gov.au/australia/radar/#{state.downcase}_radar_sites_table.shtml"))
+        currentState = State.new
+        currentState.name = state
+        @result << currentState
+        currentStation = nil
+        doc.css('table.generic tr').each do |row|
+          row.css('td').each do |cell|
+            anchor = cell.css('a').first
+            content = cell.content.strip
+            if anchor == nil && content.length > 4 #need a better check here
+              currentStation = Location.new
+              currentStation.name = content
+              currentState.locations << currentStation
+            elsif currentStation != nil && anchor != nil
+              locationView = LocationView.new
+              locationView.name = content
+              locationView.code = /IDR(\d\d\w)\./.match(anchor['href'])[1] # Get code from url
+              currentStation.views << locationView
+            end
 
-    @result = Array.new
-    ["WA", "NSW", "VIC", "QLD", "NT", "TAS", "SA" ].sort().each do |state|
-      doc = Nokogiri::HTML(open("http://www.bom.gov.au/australia/radar/#{state.downcase}_radar_sites_table.shtml"))
-      currentState = State.new
-      currentState.name = state
-      @result << currentState
-      currentStation = nil
-      doc.css('table.generic tr').each do |row|
-        row.css('td').each do |cell|
-          anchor = cell.css('a').first
-          content = cell.content.strip
-          if anchor == nil && content.length > 4 #need a better check here
-            currentStation = Location.new
-            currentStation.name = content
-            currentState.locations << currentStation
-          elsif currentStation != nil && anchor != nil
-            locationView = LocationView.new
-            locationView.name = content
-            locationView.code = /IDR(\d\d\w)\./.match(anchor['href'])[1] # Get code from url
-            currentStation.views << locationView
           end
-
         end
       end
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @result }
+        format.json { render :json => @result }
+      end
     end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @result }
-      format.json { render :json => @result }
-    end
-
-
-  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 BomFetcher::Application.routes.draw do
-  get "stations/index"
+  match "stations" => "stations#all"
+  #get "stations/all"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
