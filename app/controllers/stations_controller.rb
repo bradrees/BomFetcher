e772013e1f7034193510d939ca149028d0e0c6bd
Bradley Rees <sourcetree@bradleyrees.com>
2012-06-22 14:53:23 +0800

    Initial commit, with working station reader.

---
+++ app/controllers/stations_controller.rb
@@ -0,0 +1,62 @@
+require 'open-uri'
+require 'nokogiri'
+
+class StationsController < ApplicationController
+  def index
+
+    @result = Array.new
+    ["WA", "NSW", "VIC", "QLD", "NT", "TAS", "SA" ].sort().each do |state|
+      doc = Nokogiri::HTML(open("http://www.bom.gov.au/australia/radar/#{state.downcase}_radar_sites_table.shtml"))
+      currentState = State.new
+      currentState.name = state
+      @result << currentState
+      currentStation = nil
+      doc.css('table.generic tr').each do |row|
+        row.css('td').each do |cell|
+          anchor = cell.css('a').first
+          content = cell.content.strip
+          if anchor == nil && content.length > 4 #need a better check here
+            currentStation = Location.new
+            currentStation.name = content
+            currentState.locations << currentStation
+          elsif currentStation != nil && anchor != nil
+            locationView = LocationView.new
+            locationView.name = content
+            locationView.code = /IDR(\d\d\w)\./.match(anchor['href'])[1] # Get code from url
+            currentStation.views << locationView
+          end
+
+        end
+      end
+    end
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @result }
+      format.json { render :json => @result }
+    end
+
+
+  end
+end
+
+
+class State
+  attr_accessor :name, :locations
+
+  def initialize
+    @locations = Array.new
+  end
+end
+
+class Location
+  attr_accessor :name, :views
+
+  def initialize
+    @views = Array.new
+  end
+end
+
+class LocationView
+  attr_accessor :name, :code
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BomFetcher::Application.config.secret_token = '8786f0dfa735505afa3cfd9224f21b19e9dcecdd308faecf281817e68ce2d6990c0e2c9691fb075bb31b6edcfd471c455c4d5cf7c24df321b80f63503d618732'
